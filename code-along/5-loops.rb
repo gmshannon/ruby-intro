b20f0e93a9dfda6c2f534f00ea5d6f1b1b034011
Greg Shannon <greg.shannon@kellogg.northwestern.edu>
2025-01-28 21:58:25 +0000

    Completed exercise 5

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -2,10 +2,31 @@
 # is the same as where this file is located and then run:
 # ruby 5-loops.rb
 
+index = 0
+
 # Infinite Tacos -> Finite Tacos
-# loop do
-#   puts "tacos!"
-# end
+loop do
+    if index == 5
+        break
+    end
+
+  puts "tacos!"
+
+  index = index + 1
+end
 
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+position = 0
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+loop do 
+    if position == tacos.length
+        break
+    end
+
+    taco = tacos[position]
+    puts "#{taco} tacos!"
+    position = position + 1
+
+end
+
