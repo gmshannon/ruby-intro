045cd7134805d9194c309345e10fd36cc478d0a2
Greg Shannon <greg.shannon@kellogg.northwestern.edu>
2025-01-28 21:08:53 +0000

    Completed exercise 4

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -4,6 +4,26 @@
 
 # Hashes are lists of *key-value pairs*
 
+me = {"name" => "Greg", "location" => "Evanston", "status" => "ENTR-451 at Kellogg!"}
+puts me
+p me
+
 # Accessing data from the hash
 
+location = me["location"]
+puts location
+
 # More Complex Hashes
+
+my_profile = {
+    "name" => "Greg",
+    "location" => {"city" => "Evanston", "state" => "Illinois"},
+    "status" => "Kellogg"
+}
+puts my_profile
+puts my_profile["location"]["city"]
+p my_profile["location"]["city"]
+p my_profile["location"]["state"]
+
+my_profile["name"] = "Greg Shannon"
+puts my_profile
